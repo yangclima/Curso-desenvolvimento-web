cc8a9b4104f31153a4bdfaa7aff502ef1621c2dc
yangclima <yangclima@gmail.com>
2024-06-24 22:10:09 -0300

    Finalização do primeiro projeto front-end, criação de uma interface de login/cadastro

---
+++ PP - 01/function.js
@@ -0,0 +1,9 @@
+function SetLoginDisplayNone() {
+    document.getElementById("login").style.tranform = "none";
+    document.getElementById("registration").style.display = "unset";
+}
+
+function SetRegistrationDisplayNone() {
+    document.getElementById("login").style.display = "unset";
+    document.getElementById("registration").style.display = "none";
+}
